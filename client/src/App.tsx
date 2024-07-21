1f57c80c5b81dc67e0c9a03a1b119f4636d3858c
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-07-21 15:00:01 +0700

    initial client project

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello World!</h1>
+        <button className="btn btn-primary">create</button>
+      </div>
+    </>
+  )
+}
+
+export default App
