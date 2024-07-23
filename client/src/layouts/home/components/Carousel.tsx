60ea9cd0e57122f05304692bf83530e45960b06b
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-07-23 21:09:12 +0700

    Update Carousel.tsx

--- client/src/layouts/home/components/Carousel.tsx
+++ client/src/layouts/home/components/Carousel.tsx
@@ -8,7 +8,7 @@ export default function Carousel() {
                 <div className="homepage-carousel-title">
                     <h3>Find your next "I stayed up too late reading book"</h3>
                 </div>
-                <div id="carouselExampleControls" className="carousel carousel-dark slide mt-5 d-none d-lg-block" data-bs-interval="false">
+                <div id="carouselExampleControls" className="carousel carousel-dark slide mt-5 d-none d-lg-block " data-bs-interval="false">
                     {/* desktop */}
                     <div className="carousel-inner">
                         <div className="carousel-item active">
