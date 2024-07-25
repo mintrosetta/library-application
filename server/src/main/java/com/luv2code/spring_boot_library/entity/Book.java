b602625e200b0b341a5eec145f5efe130a462c11
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-07-26 00:13:06 +0700

    create book entity

---
+++ server/src/main/java/com/luv2code/spring_boot_library/entity/Book.java
@@ -0,0 +1,39 @@
+package com.luv2code.spring_boot_library.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "book")
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private Integer copies;
+
+    @Column(name = "copies_avaliable")
+    private Integer copiesAvaliable;
+
+    @Column(name = "img")
+    private String img;
+
+}
